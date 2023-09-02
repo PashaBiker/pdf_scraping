c447e98065af9b5c2fc9b0e77434cea2c12529d5
Yankevych <pawabiker@gmail.com>
2023-09-02 15:54:59 +0300

    updated 15 milestone

--- 15_milestone/Vanguard/extract_Vanguard.py
+++ 15_milestone/Vanguard/extract_Vanguard.py
@@ -10,170 +10,117 @@ import time
 import PyPDF2
 import pdfplumber
 import io
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-excel_file = 'Vanguard.xlsm'
-pdf_folder = 'Vanguard PDFs'
+excel_file = '15_milestone\Vanguard\Vanguard.xlsm'
 
-def get_data(file):
+def get_data(url):
 
-    date = ''
-    AMC = None
-    first_line_found = False
-    one_year = None
-    one_month = None
-    try:
-        with pdfplumber.open(file) as pdf:
-            first_line_found = False
-            first_AMC_found = False
-            for page in pdf.pages:
-                text = page.extract_text()
-                text = text.split('\n')
-                # print(text)
+    if 'shares/' in url:
+        base_url = url.split('shares/')[0] + 'shares/'
+        # print(base_url)
+        
+    price_performance_url = base_url + "price-performance"
+    driver = webdriver.Chrome()
+    driver.get(price_performance_url)
 
-                for line in text:
+    # Wait until the specific element appears and click the button
+    element_present = EC.presence_of_element_located((By.XPATH, '//*[@id="main-content"]/ukd-fund-detail/div[2]/ukd-price-performance/ukd-past-performance/ukd-link-tabs/div/div/div/nav/button[6]'))
+    element = WebDriverWait(driver, 30).until(element_present)
 
-                    if 'as at' in line:
-                        # print(line)
-                        date_match = re.search(r'(\d{1,2}\s\w+\s\d{4})', line)
-                        if date_match:
-                            date = date_match.group(1)
+    button = driver.find_element(By.XPATH, '//*[@id="main-content"]/ukd-fund-detail/div[2]/ukd-price-performance/ukd-past-performance/ukd-link-tabs/div/div/div/nav/button[6]')
+    scroll_to_past_perf = driver.find_element(By.XPATH, '//*[@id="main-content"]/ukd-fund-detail/div[2]/ukd-price-performance/ukd-past-performance/h3')
+    driver.execute_script("arguments[0].scrollIntoView();", scroll_to_past_perf)
+    time.sleep(2)  # Дайте немного времени после скролла перед тем, как кликнуть
+    button.click()
+    # Extract and print the table content
+    table = driver.find_element(By.CLASS_NAME, 'responsive-scrollable-table')
+    table_text = table.text
+    one_month_one_year_text = table_text.split('\n')
 
-                    if 'Class B GBP' in line:
-                        # print(line)
-                        AMC_number = re.findall(r'\d+\.?\d*', line)
-                        AMC = AMC_number[1]
+    ocf = driver.find_element(By.XPATH,'//*[@id="main-content"]/ukd-fund-detail/div[2]/ukd-price-performance/ukd-past-performance/ukd-link-tabs/div/span')
+    ocf_text = ocf.text
 
-                    if 'DISCRETE ANNUAL PERFORMANCE' in line:
-                        first_line_found = True
-                        continue
-
-                    if first_line_found:
-                        # Проверка на отсутствие даты в строке
-                        if not re.search(r'\d+\.\d+\.\d+', line):
-                            # Поиск чисел с десятичной точкой
-                            numbers = re.findall(r'[-+]?\d+\.\d+', line)
-                            if len(numbers) >= 4:
-                                if one_year is None:
-                                    one_year = numbers[0]
-                                elif one_month is None:
-                                    one_month = numbers[0]
-
-            # print(date)
-            # print(AMC)
-            # print(one_year)
-            # print(one_month)
+    date = driver.find_element(By.XPATH,'//*[@id="main-content"]/ukd-fund-detail/div[2]/ukd-price-performance/ukd-past-performance/p[1]')
+    date_text = date.text
 
 
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
 
-    asset_labels = ['Equities',
-                    'Fixed Income',
-                    'Diversifiers',
 
-                    'Global STAR Equities',
-                    'North American',
-                    'Europe ex UK',
-                    'UK',
-                    'Equity Themes',
-                    'Emerging Markets',
+    # Navigate to the portfolio-data URL
+    portfolio_data_url = base_url + "portfolio-data"
+    driver.get(portfolio_data_url)
 
-                    'Government & Supranational',
+    # Wait until the specific element appears
+    element_present_portfolio = EC.presence_of_element_located((By.XPATH, '//*[@id="main-content"]/ukd-fund-detail/div[2]/ukd-portfolio-data/ukd-underlying-allocations/div'))
+    element_portfolio = WebDriverWait(driver, 30).until(element_present_portfolio)
 
-                    'Corporate Senior',
-                    'Corporate Subordinated',
+    # Extract and print the table content
+    table_portfolio = driver.find_element(By.XPATH, '//*[@id="main-content"]/ukd-fund-detail/div[2]/ukd-portfolio-data/ukd-underlying-allocations/div/div/table')
+    assets_text = table_portfolio.text.split('\n')
+    # Close the driver
+    driver.quit()
 
-                    'Financials Senior',
-                    'Financials Subordinated',
-                    'Cash/derivatives',
+    # print(date_text)
+    date = date_text.replace("As at date ", "")
+    print(date)
+    # print(ocf_text)
+    OCF = re.search(r'(\d+\.\d+)', ocf_text).group(1)
+    print(ocf)
+    # print(one_month_one_year_text[2])
+    numbers = re.findall(r'[-+]?\d+\.\d+', one_month_one_year_text[2])
+    one_month = numbers[0]
+    one_year = numbers[3]
+    print(one_month)
+    print(one_year)
+    # print(assets_text)
 
-                    'Alternative Funds',
-                    'Cash',
-                    'Gold',
-                    'Commodities',]
+    # breakpoint()
+
+    asset_labels = [
+            'Vanguard Global Bond Index Fund GBP Hedged Acc',
+	        'Vanguard FTSE Developed World ex-U.K. Equity Index Fund GBP Acc',
+        	'Vanguard Global Aggregate Bond UCITS ETF GBP Hedged Accumulating',
+            'Vanguard U.K. Government Bond Index Fund GBP Acc',
+            'Vanguard U.K. Investment Grade Bond Index Fund GBP Acc',
+            'Vanguard U.K. Inflation-Linked Gilt Index Fund GBP Acc',
+            'Vanguard U.S. Government Bond Index Fund GBP Hedged Acc',
+            'Vanguard U.S. Investment Grade Credit Index Fund GBP Hedged Acc',
+            'Vanguard Euro Government Bond Index Fund GBP Hedged Acc',
+            'Vanguard FTSE U.K. All Share Index Unit Trust GBP Acc',	
+            'Vanguard Euro Investment Grade Bond Index Fund GBP Hedged Acc',	
+            'Vanguard Japan Government Bond Index Fund GBP Hedged Acc',	
+            'Vanguard Emerging Markets Stock Index Fund GBP Acc',	
+            'Vanguard U.S. Equity Index Fund GBP Acc',
+            'Vanguard S&P 500 UCITS ETF (USD) Accumulating',	
+            'Vanguard FTSE Developed Europe ex-U.K. Equity Index Fund GBP Acc',	
+            'Vanguard FTSE 100 UCITS ETF (GBP) Accumulating',	
+            'Vanguard Japan Stock Index Fund GBP Acc',	
+            'Vanguard Pacific ex-Japan Stock Index Fund GBP Acc',	
+            'Vanguard USD Corporate Bond UCITS ETF GBP Hedged Acc',	
+            'Vanguard FTSE 250 UCITS ETF (GBP) Accumulating',]
 
     asset_labels = sorted(asset_labels, key=lambda x: len(x), reverse=True)
 
-    with pdfplumber.open(file) as pdf:
-        for i, page in enumerate(pdf.pages):
-            if i == 0:  # Если это первая страница
-                # Обрезать верхнюю половину по высоте и 62.5% левой части по ширине
-                right_bottom_quarter = page.crop((page.width * 0.625, page.height / 2, page.width, page.height))
-                text = right_bottom_quarter.extract_text()
-                
-                # Разделяем текст на строки и добавляем их в список
-                lines = text.split('\n')
-
-            elif i == 1:  # Если это вторая страница
-                # Обрезать 37.5% левой стороны страницы
-                right_part = page.crop((page.width * 0.375, 0, page.width, page.height))
-                text = right_part.extract_text()
-                
-                # Разделяем текст на строки и добавляем их в список
-                lines.extend(text.split('\n'))
-            else:
-                # Если страниц больше двух, завершите цикл
-                break
-        print(lines)
-        # Initialize the asset_values dictionary
-        asset_values = {word: 0 for word in asset_labels}
-        lines_to_remove = []
-        total = 0.0
-        for line in lines:
-            matched = False
-            # Для каждой метки актива в asset_labels проверяем, содержится ли она в строке
-            for label in asset_labels:
-                if label in line:
-                    # Если содержится, то используем регулярное выражение, чтобы извлечь числовое значение
-                    value = re.search(r'(\d+\.\d+)', line.replace(label, ''))  # удаляем метку, чтобы избежать конфликтов
-                    if value:
-                        asset_values[label] = float(value.group(1))
-                        total += float(value.group(1))
-                        matched = True
-                        lines_to_remove.append(line)
-                        break
-            if matched:
-                asset_labels.remove(label)
-        
-        # Удаляем обработанные строки
-        for line in lines_to_remove:
-            lines.remove(line)
-        categories = ["Corporate", "Financials"]
-        current_category = None
-
-        for i, line in enumerate(lines):
-            for category in categories:
-                if category in line:
-                    current_category = category
-
-            if "Senior" in line:
-                value = re.search(r'(\d+\.\d+)', line)
-                if value:
-                    key = f"{current_category} Senior"
-                    asset_values[key] = float(value.group(1))
-
-            if "Subordinated" in line:
-                value = re.search(r'(\d+\.\d+)', line)
-                if value:
-                    key = f"{current_category} Subordinated"
-                    asset_values[key] = float(value.group(1))    
-        print(asset_values)
-        print(total)
- 
-    return one_year,one_month, asset_values, AMC, date
+    asset_values = {word: 0 for word in asset_labels}
+    total = 0.0
+    for line in assets_text:
+        for label in asset_labels:
+            if label in line:
+                match = re.search(r'(\d+\.\d+)%', line)
+                if match:
+                    asset_values[label] = float(match.group(1))
+                    total += float(match.group(1))
+    print(asset_values)
+    print(total)
+    return one_year,one_month, asset_values, OCF, date
     # return asset_values, date
 
-
-def clean_text(text):
-
-    cleaned_line = re.sub(r'\d+\.?\d*', '', text)
-
-    # Remove all spaces from the line and the word being searched
-    cleaned_text = ''.join(cleaned_line.split())
-    return cleaned_text
-
-
-def write_to_sheet(one_year,one_month, assets, AMC, spreadsheet, filename, date):
+def write_to_sheet(one_year,one_month, assets, OCF, spreadsheet, filename, date):
 
     try:
         app = xw.App(visible=False)
@@ -192,7 +139,7 @@ def write_to_sheet(one_year,one_month, assets, AMC, spreadsheet, filename, date)
                 cellb.value = date
 
                 cellc = sheet.range('C'+str(i+1))
-                cellc.value = float(AMC)/100
+                cellc.value = float(OCF)/100
                 cellc.number_format = '0.00%'
 
                 celld = sheet.range('D'+str(i+1))
@@ -245,6 +192,28 @@ def write_to_sheet(one_year,one_month, assets, AMC, spreadsheet, filename, date)
         wb.close()
         app.quit()
 
+def filenames(spreadsheet):
+    
+    app = xw.App(visible=False)
+    wb = app.books.open(spreadsheet, update_links=False, read_only=False)
+    sheet = wb.sheets[1]
+    filenames_list = []
+    urls_list = []
+    # Start from row 3 and iterate through the links in column B
+    last_row = sheet.range('B' + str(sheet.cells.last_cell.row)).end('up').row
+    for row in range(3, last_row+1):
+        # Get the corresponding filename from column A
+        filename = sheet.range(f'A{row}').value
+        filenames_list.append(filename)
+        url = sheet.range(f"B{row}").value
+        urls_list.append(url)
+        # Download the PDF from the link
+    
+    wb.close()
+    app.quit()
+    # print(filenames_list)
+    # print(urls_list)
+    return filenames_list, urls_list
 
 def column_letter_from_index(index):
     result = ""
@@ -258,26 +227,13 @@ def column_letter_from_index(index):
 
 if __name__ == '__main__':
 
-    # enter the name of the excel file
+    
+    filenames, urls = filenames(excel_file)
 
-# TODO UNCOMENT FIRST
-# TODO UNCOMENT FIRST
-# TODO UNCOMENT FIRST
+    for filename, url in zip(filenames, urls):
+        one_year, one_month, assets, OCF, date = get_data(url)
+        write_to_sheet(one_year, one_month, assets, OCF, excel_file, filename, date)
 
-    pdfs = glob.glob(pdf_folder + '/*.pdf')
-
-    for pdf in pdfs:
-        try:
-            one_year,one_month, assets, AMC, date = get_data(pdf)
-            # TODO: FILENAMES
-            # TODO: FILENAMES
-            # TODO: FILENAMES
-            write_to_sheet(one_year,one_month, assets, AMC, excel_file,pdf.split('\\')[-1].split('.')[0], date)
             # write_to_sheet(one_year,one_month, assets, AMC, excel_file,pdf.split('\\')[-1].split('.')[0], date)
-
-        except Exception as e:
-            print(f"An error occurred in file {pdf}: {str(e)}")
-
-        # print('Portfolio Cost:', portfolio_cost + '\t' + pdf)
 
     print('\nDone!')
